9a480167b645f82e212794ddd327db9679321b9c
stephencockerill <stephencockerill66@gmail.com>
2018-05-28 20:52:23 -0700

    move images dir into docs

--- build.py
+++ build.py
@@ -1,16 +1,19 @@
 import os
 
-content_dir = 'content'
-docs_dir = 'docs'
+def main():
+    content_dir = 'content'
+    docs_dir = 'docs'
 
-top = open('templates/top.html', 'r').read()
-bottom = open('templates/bottom.html', 'r').read()
+    top = open('templates/top.html', 'r').read()
+    bottom = open('templates/bottom.html', 'r').read()
 
-for filename in os.listdir(content_dir):
-    if filename.endswith('.html'):
-        content = open('%s/%s' % (content_dir, filename), 'r').read()
-        page_content = top + content + bottom
-        page_file = open('%s/%s' % (docs_dir, filename), 'w')
-        page_file.write(page_content)
-        page_file.close()
+    for filename in os.listdir(content_dir):
+        if filename.endswith('.html'):
+            content = open('%s/%s' % (content_dir, filename), 'r').read()
+            page_content = top + content + bottom
+            page_file = open('%s/%s' % (docs_dir, filename), 'w')
+            page_file.write(page_content)
+            page_file.close()
 
+if __name__=='__main__':
+    main()
